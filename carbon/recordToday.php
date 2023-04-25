048f90f1d911956d4e81b27de76c219065a7117d
eason <410401090@m365.fju.edu.tw>
2023-04-25 09:01:52 +0800

    2023/04/25 09:00 delete finish the table of Today's record.

--- carbon/recordToday.php
+++ carbon/recordToday.php
@@ -4,5 +4,12 @@
     $currentUserID = $_SESSION['userID'];
     $link = @mysqli_connect('localhost', 'root', '12345678', 'sa');
     $sql = "SELECT * FROM account WHERE userID = '$currentUserID' and recordTime = '$currentDate'";
+    $result = mysqli_query($link, $sql);
     echo '<table><tr><th scope="row">紀錄時間</th><th>一次性餐具所產生之碳排放量</th><th scope="row">交通所產生之碳排放量</th><th scope="row">獲得點數</th></tr>';
-    ?>
+    while($row = mysqli_fetch_assoc($result)){
+    $getPoint = $row['tablewarePointAddition'] + $row['trafficPointAddition'] - $row['tablewareDeduction'] - $row['trafficDeduction'];
+    echo '<tr><td>', $row['recordTime'], '</td><td>', $row['tablewareCarbon'], '</td><td>', $row['trafficCarbon'], '</td><td>', $getPoint, '</td></tr>';
+    }
+    echo '</table>';
+
+?>
