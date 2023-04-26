67dc2c71623504e67dfd5bfc334ddd8b0001da74
eason <410401090@m365.fju.edu.tw>
2023-04-26 17:17:25 +0800

    2023/04/26 17:17 end

--- carbon/logout.php
+++ carbon/logout.php
@@ -1,29 +1,10 @@
 <?php
-include('config.php');
-$userID = $_GET['userID'];
-$password =$_GET['password'];
-
-$sql ="select * from account where userID = '$userID' and password = '$password'";
-
-$result=mysqli_query($link,$sql);
-if($row = mysqli_fetch_assoc($result))
-{
-    header("location: home.php");?>
-    <script language="javascript">
-    alert('登出成功');
-    history.back();
-    </script>
-    <?php
-}
-else{
-
-    ?>
-
-<script language="javascript">
-    alert('登出失敗');
-    history.back();
-</script>
-
-<?php
-}
+session_start();
+$_SESSION['userID'] = "";
+$_SESSION['name'] = "";
+$_SESSION['level'] = "";
 ?>
+<script language="javascript">
+    alert('登出成功');
+    location.href="home.php";
+</script>
