ebe13bbd2210d7831f8f0cd77696828358db91d1
蔡雯如 <a0918316411@gmail.com>
2023-04-26 14:13:19 +0800

    20230426

---
+++ carbon/sign_b.php
@@ -0,0 +1,25 @@
+<?php
+include('config.php');
+
+$name=$_GET['name'];
+$phone=$_GET['phone'];
+$userID=$_GET['userID'];
+$password=$_GET['password'];
+
+
+$sql="INSERT into account (name,phone,userID,password) values ('$name','$phone','$userID','$password')";
+$result=mysqli_query($link,$sql);
+if($result){?>
+<script language="javascript">
+    alert('註冊成功');
+    history.back();
+    </script>
+    <?php
+    header("location:login.php");
+}else{?>
+   <script language="javascript">
+    alert('帳密錯誤');
+    history.back();
+    </script>
+<?php
+}?>
