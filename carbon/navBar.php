6d407fd0278b7af2af336570137a514309913cb8
eason <410401090@m365.fju.edu.tw>
2023-04-26 17:11:08 +0800

    2023/04/26 17:10 end

--- carbon/navBar.php
+++ carbon/navBar.php
@@ -8,7 +8,7 @@ if($_SESSION['level'] == 'user'){
     <li><a href="count1.php">計算碳排</a></li>
     <li><a href="history.php">歷史紀錄</a></li>
     <li><a href="personal.php">個人資料管理</a></li>
-    <li><a href="login.php"><font style="color:#70c745;">登入</font></a></li>
+    <li><a href="logout.php"><font style="color:#70c745;">Hi&nbsp&nbsp <?php echo $_SESSION['name']."  登出"?></font></a></li>
     </ul>
     </div>
 <?php
@@ -19,7 +19,7 @@ if($_SESSION['level'] == 'user'){
                                            <li></li>
                                            <li></li>
                                            <li></li>
-                                           <li><a href="login.php"><font style="color:#70c745;">登入</font></a></li>
+                                           <li><a href="login.php"><font style="color:#70c745;">登入|註冊</font></a></li>
                                       </ul>
                                 </div>
                                 <?php
