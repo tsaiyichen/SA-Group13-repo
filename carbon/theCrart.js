9c742cd5468283ff89a7ef0de5ee4698f1c743e8
eason <410401090@m365.fju.edu.tw>
2023-04-27 11:45:55 +0800

    2023/04/27 barChart

---
+++ carbon/theCrart.js
@@ -0,0 +1,58 @@
+$.ajax({
+    url: 'theChart_b.php',
+    method: 'GET',
+    dataType: 'json',
+    success: function(response){
+        var ctx = document.getElementById("myChart").getContext("2d");
+
+        // 資料
+        var data = {
+            labels: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+            datasets: [
+                {
+                    label: "資料 1",
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderWidth: 1,
+                    data: response.table
+                },
+                {
+                    label: "資料 2",
+                    backgroundColor: "rgba(255, 99, 132, 0.4)",
+                    borderColor: "rgba(255,99,132,1)",
+                    borderWidth: 1,
+                    data: response.traffic
+                },
+                {
+                    label: "總和",
+                    type: "line",
+                    fill: false,
+                    borderColor: "rgba(255, 206, 86, 1)",
+                    borderWidth: 2,
+                    data: response.sum
+                }
+            ]
+        };
+
+        // 選項
+        var options = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        };
+
+        // 長條圖和折線圖
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: options
+        })
+    },
+    error: function() { // 當取得資料失敗時
+        alert("取得資料失敗！");
+    }
+});
---
+++ carbon/theCrart_b.php
@@ -0,0 +1,121 @@
+<?php
+    $link = @mysqli_connect('localhost', 'root', '12345678', 'sa');
+    $tableSum = 0;
+    $trafficSum = 0;
+    $datatable = [];
+    $dataTraffic = [];
+    $currentUserID = "yyt0313";
+    date_default_timezone_set("Asia/Taipei");
+    $currentYear = date('Y');
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-01%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[0] = $tableSum; $dataTraffic[0] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-02%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[1] = $tableSum; $dataTraffic[1] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-03%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[2] = $tableSum; $dataTraffic[2] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-04%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[3] = $tableSum; $dataTraffic[3] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-05%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[4] = $tableSum; $dataTraffic[4] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-06%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[5] = $tableSum; $dataTraffic[5] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-07%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[6] = $tableSum; $dataTraffic[6] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-08%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[7] = $tableSum; $dataTraffic[7] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-09%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[8] = $tableSum; $dataTraffic[8] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-10%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[9] = $tableSum; $dataTraffic[9] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-11%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[10] = $tableSum; $dataTraffic[10] = $trafficSum;
+    $tableSum = 0;
+    $trafficSum = 0;
+    $sql = "SELECT * FROM record WHERE userID = '$currentUserID' and recordTime LIKE '$currentYear-12%'";
+    $result = mysqli_query($link, $sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $tableSum += $row['tablewareCarbon'];
+        $trafficSum += $row['trafficCarbon'];
+    }
+    $datatable[11] = $tableSum; $dataTraffic[11] = $trafficSum;
+    $sum = [];
+    for($i = 0; $i < 12; $i++){
+        $sum[$i] = $datatable[$i] + $dataTraffic[$i];
+    }
+echo  json_encode(array('table' => $datatable, 'traffic' => $dataTraffic,'sum' => $sum));
+?>
