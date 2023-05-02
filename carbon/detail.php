cc4035d1ca994ea24c917b93adb6a7d99b5117bf
agnes <agnesyu0110@gmail.com>
2023-05-02 14:29:58 +0800

    detail前端

---
+++ carbon/detail.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Carbon</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+        <!-- plugins:css -->
+        <link rel="stylesheet" href="../../assets/vendors/mdi/css/materialdesignicons.min.css">
+        <link rel="stylesheet" href="../../assets/vendors/css/vendor.bundle.base.css">
+        <!-- endinject -->
+        <!-- Plugin css for this page -->
+        <!-- End plugin css for this page -->
+        <!-- inject:css -->
+        <!-- endinject -->
+        <!-- Layout styles -->
+        <link rel="stylesheet" href="../../assets/css/style.css">
+
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img">
+            <img src="img/core-img/leaf.png" alt="">
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+
+
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
+
+                        <!-- Nav Brand -->
+                        <a href="home.php" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+
+
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <section class="hero-area">
+        <div class="hero-post-slides owl-carousel">
+
+            <!-- Single Hero Post -->
+            <div class="single-hero-post bg-overlay">
+                <!-- Post Image -->
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/1.jpg);"></div>
+
+
+
+                            <!-- Post Content -->
+
+    <style>
+       #table-wrapper {
+           position: absolute;
+           top: 40%;
+           left: 50%;
+           transform: translate(-50%, -50%);
+        }
+        span {
+                      font-weight: normal;
+                      font-size:20px;
+                      color: gray;
+                     }
+    </style>
+    <style>
+    .btn-green {
+      background-color: green;
+      color: white;
+    }
+    </style>
+
+    <div class="container-scroller "  style="width:700px;" id="table-wrapper">
+         <div class="container-fluid page-body-wrapper full-page-wrapper">
+             <div class="card w-300 h-200 ">
+                 <div class="card-body px-5 py-5">
+                 <div class="row align-items-start">
+                    <h1>新怪獸！<span>2023-4-30</span></h1>
+
+                        </div>
+                        <p align="left">
+                        同學們好：
+
+                        如果您對於未來感到徬徨、想要超前部署未來就業或升學，踏上外商與一流企業職涯之路。歡迎您報名「ERP顧問實習」。
+
+                        根據多年的實例驗證，同學透過這樣的實習，確實「翻轉人生」，是值得推薦的5星級實習。
+
+                        今年除了7~8月應用顧問的實習外，更新增「SAP ABAP Team 實習」，是對於有志於到外企從事顧問工程師的一條「綠色通道」。
+
+                        🔥報名實習即將於2023/5/16(二)截止，同學們可把握時間！不要錯過站上舞台發光的機會。
+                        </p>
+                        <div style="text-align: center;">
+                     <button type="button" class="btn btn-outline-success" style="float: left;">修改</button>
+                      <button type="button" class="btn btn-outline-success" style="float: right;">刪除</button>
+                       </div>
+
+
+                 </div>
+             </div>
+         </div>
+    </div>
+
+    </div>
+    </div>
+
+    </section>
+    <!-- ##### Hero Area End ##### -->
+
+    <!-- ##### All Javascript Files ##### -->
+    <!-- jQuery-2.2.4 js -->
+
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../../assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../../assets/js/off-canvas.js"></script>
+    <script src="../../assets/js/hoverable-collapse.js"></script>
+    <script src="../../assets/js/misc.js"></script>
+    <script src="../../assets/js/settings.js"></script>
+    <script src="../../assets/js/todolist.js"></script>
+    <!-- endinject -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
