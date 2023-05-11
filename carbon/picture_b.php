d31e09e8de3e9e16f7ee91a17ceb07d5cc4d192e
蔡雯如 <a0918316411@gmail.com>
2023-05-11 20:25:13 +0800

    20230426(後端)

---
+++ carbon/picture_b.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$link = @mysqli_connect('localhost', 'root', '12345678', 'sa');
+$monsterID = $_GET['monsterID'];
+
+if ($_SESSION['level'] == 'user') {
+    $userID = $_SESSION['userID'];
+    $sql = "select * from monster,purchase where monster.monsterID=purchase.monsterID and userID='$userID'";
+} else {
+    $sql = "select * from monster";
+}
+$result = mysqli_query($link, $sql);
+?>
