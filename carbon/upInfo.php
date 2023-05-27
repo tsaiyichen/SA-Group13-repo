9860976f20a2a732307d69bb5a68b256d8eb2496
蔡雯如 <a0918316411@gmail.com>
2023-05-27 22:18:39 +0800

    20230527把前端upInfo完成

--- carbon/upInfo.php
+++ carbon/upInfo.php
@@ -20,6 +20,9 @@
 </head>
 
 <body>
+
+
+
     <!-- Preloader -->
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-circle"></div>
@@ -33,54 +36,56 @@
 
 
 
-            <!-- ***** Navbar Area ***** -->
-            <div class="alazea-main-menu">
-                <div class="classy-nav-container breakpoint-off">
-                    <div class="container">
-                        <!-- Menu -->
-                        <nav class="classy-navbar justify-content-between" id="alazeaNav">
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
 
-                            <!-- Nav Brand -->
-                            <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+                        <!-- Nav Brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
 
-                            <!-- Navbar Toggler -->
-                            <div class="classy-navbar-toggler">
-                                <span class="navbarToggler"><span></span><span></span><span></span></span>
-                            </div>
-
-                            <!-- Menu -->
-                            <div class="classy-menu">
-
-                                <!-- Close Button -->
-                                <div class="classycloseIcon">
-                                    <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                                </div>
-
-                                <!-- Navbar Start -->
-                                <?php include "navBar.php";?>
-                                <!-- Navbar End -->
-                            </div>
-                        </nav>
-
-                        <!-- Search Form -->
-                        <div class="search-form">
-                            <form action="#" method="get">
-                                <input type="search" name="search" id="search" placeholder="Type keywords &amp; press enter...">
-                                <button type="submit" class="d-none"></button>
-                            </form>
-                            <!-- Close Icon -->
-                            <div class="closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
                         </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+                            <?php include "navBar.php"; ?>
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+
+                    <!-- Search Form -->
+                    <div class="search-form">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search"
+                                placeholder="Type keywords &amp; press enter...">
+                            <button type="submit" class="d-none"></button>
+                        </form>
+                        <!-- Close Icon -->
+                        <div class="closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
+    </header>
     <!-- ##### Header Area End ##### -->
 
     <!-- ##### Breadcrumb Area Start ##### -->
     <div class="breadcrumb-area">
         <!-- Top Breadcrumb Area -->
-        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(img/bg-img/24.jpg);">
+        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center"
+            style="background-image: url(img/bg-img/24.jpg);">
             <h2>最新環保資訊發佈</h2>
         </div>
 
@@ -106,19 +111,22 @@
                 <!-- Contact Thumbnail -->
                 <div class="col-12 col-md-6">
 
-                        <img src="img/bg-img/25.jpg" alt="">
+                    <p style="position: absolute;top: -40px;left: 130px;color: green;font-size: 35px;">最新環保資訊發佈</p>
+                    <img src="img/bg-img/25.jpg" alt="">
 
                 </div>
 
                 <div class="col-12 col-md-6">
                     <!-- Section Heading -->
                     <div class="section-heading">
-                        <form method="get" action="announce_inb.php" style="font-size: 20px;"><br><br>
+                        <form method="get" action="upInfo_b.php" style="font-size: 20px;"><br><br>
+
                             標題：<br>
                             <input type="text" name="title" required style="width:450px;"><br><br>
                             內文：<br>
                             <textarea name="content" required style="width:450px;height:100px;"></textarea><br><br>
-                            <input type="submit" value="提交"  style="width: 70px;height: 40px; border-radius: 4px;background-color: #70c745; color: white; border-color:#DDDDDD ;">
+                            <input type="submit" value="提交"
+                                style="width: 70px;height: 40px; border-radius: 4px;background-color: #70c745; color: white; border-color:#DDDDDD ;">
                         </form>
 
                     </div>
