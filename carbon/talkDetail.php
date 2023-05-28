100ab5c253d86d48d9e88e7033f202ef245ba980
YYT <yungting111@gmail.com>
2023-05-28 17:33:22 +0800

    talkDetail前端

---
+++ carbon/talkDetail.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>carbon</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img">
+            <img src="img/core-img/leaf.png" alt="">
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+
+
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
+
+                        <!-- Nav Brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="shop.html">Shop</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="shop.html">Shop</a></li>
+                                                    <li><a href="shop-details.html">Shop Details</a></li>
+                                                    <li><a href="cart.html">Shopping Cart</a></li>
+                                                    <li><a href="checkout.html">Checkout</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="portfolio.html">Portfolio</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="portfolio.html">Portfolio</a></li>
+                                                    <li><a href="single-portfolio.html">Portfolio Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="blog.html">Blog</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="single-post.html">Blog Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="shop.html">Shop</a></li>
+                                    <li><a href="portfolio.html">Portfolio</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+
+                                <!-- Search Icon -->
+                                <div id="searchIcon">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </div>
+
+                            </div>
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+
+                    <!-- Search Form -->
+                    <div class="search-form">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search" placeholder="Type keywords &amp; press enter...">
+                            <button type="submit" class="d-none"></button>
+                        </form>
+                        <!-- Close Icon -->
+                        <div class="closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Breadcrumb Area Start ##### -->
+    <div class="breadcrumb-area">
+        <!-- Top Breadcrumb Area -->
+        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(img/bg-img/24.jpg);">
+            <h2>文章討論區</h2>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> 首頁</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">文章討論區</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Breadcrumb Area End ##### -->
+
+    <!-- ##### Cart Area Start ##### -->
+    <div class="cart-area section-padding-0-100 clearfix">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="cart-table clearfix">
+                        <table class="table table-responsive">
+                            <thead>
+                                <tr>
+                                    <h3>#標題</h3>
+                                </tr>
+                                <tr>
+                                    <p style="display:inline;">發文者：TYC&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2023/5/28&nbsp;&nbsp;&nbsp;23:11</p>
+                                </tr>
+
+
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td >
+
+                                        <p>內文：先講結論，後來有一起走完3天2夜的行程，平安回台
+                                            以下針對前一篇文章回應
+
+                                            餐廳是香港阿甘蝦餐廳，大家去查菜單就可以發現並非"全蝦餐廳"，還是有很多與蝦無關的餐點，原po說她有拍當天的餐點，不如請她po照片看看是否餐點全是蝦??全蝦餐廳可以點到雞胸肉?連牛排都淋龍蝦醬?
+
+                                            我愛吃蝦、我一定要吃蝦、一定要去阿甘蝦餐廳、每個人一定要點餐、一定要點蝦餐點
+                                            我想這些都是不同概念，不要混為一談了
+                                            我愛吃蝦? ==>對
+                                            我一定要吃蝦? ==>不對
+                                            一定要去阿甘蝦餐廳?==>事前可以討論但沒討論，事後訂了餐廳下午5點一定要去，總不能no show吧
+                                            每個人一定要點餐? ==>對，餐廳外面就這樣公告
+                                            一定要點蝦餐點? ==>不對，看菜單還有很多非蝦餐點
+
+                                            為什麼點餐的時候我不高興?
+                                            這次3天2夜的國外旅行，事前確定的行程只有2個，一個是太平山纜車、另一個是下午5點的阿甘蝦餐廳
+                                            1. 因為假日習慣是中午起床後吃早午餐，所以早上沒吃早餐，結果11點的飛機，她因為飛機餐難吃就不吃，導致下午3點check in完飯店後喊肚子餓，臨時找一個茶餐廳填肚子，忘記下午5點還有阿甘蝦餐廳?
+                                            2. 到了太平山準備搭纜車了，問"為什麼要來這裡?"</p>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+
+                        </table>
+                        <p>文章點擊率：600次</p>
+                        <button type="submit" style="width: 100px;height: 40px; border-radius: 4px;background-color: #70c745; color: white; border-color:#DDDDDD ;">修改</button>
+                        <button type="submit" style="width: 100px;height: 40px; border-radius: 4px;background-color: #70c745; color: white; border-color:#DDDDDD ;">刪除</button>
+                    </div>
+                </div>
+            </div>
+
+            <br><br><br>
+            <div class="row">
+                <div class="col-12">
+                    <div class="cart-table clearfix">
+                        <table class="table table-responsive">
+                            <thead>
+
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <h5>討論區</h5>
+
+                                </tr>
+                                <br><br>
+                                <tr>
+                                    <h6>YYT：</h6>妳好，我覺得你說的很棒
+                                </tr><hr>
+                                <tr>
+                                    <h6>lulu：</h6>我不這麼認為啊啊啊啊啊啊啊啊啊啊啊啊啊
+                                </tr><hr>
+                                <tr>
+                                    <h6>季ㄅ：</h6>嘿嘿嘿嘿嘿
+                                </tr><hr>
+                            </tbody>
+
+                            <div class="coupon-discount mt-70">
+                            <form action="#" method="post">
+                                <input name="content" style="width: 1000px; height: 40px;" placeholder="我想說...">&nbsp;
+                                <button type="submit" style="width: 100px;height: 40px; border-radius: 4px;background-color: #70c745; color: white; border-color:#DDDDDD ;">發布</button>
+                            </form>
+                            </div>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+    <!-- ##### Cart Area End ##### -->
+
+
+
+    <!-- ##### All Javascript Files ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
