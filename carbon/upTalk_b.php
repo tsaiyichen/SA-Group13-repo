1de96d1543e99a0c408e63d6b3129cf0e7aafc69
eason <410401090@m365.fju.edu.tw>
2023-05-31 10:30:19 +0800

    2023/05/31 finish upTalk

--- carbon/upTalk_b.php
+++ carbon/upTalk_b.php
@@ -1,18 +1,17 @@
 <?php
-$userID = $_GET['userID'];
-$articleID = $_GET['articleID'];
+session_start();
+$userID = $_SESSION['userID'];
 $content = $_GET['content'];
 $title = $_GET['title'];
 
 $link = @mysqli_connect('localhost', 'root', '12345678', 'sa');
-$sql = "INSERT INTO article　(articleID, title, content, userID) VALUES ('$articleID', '$title', '$content', '$title')";
+$sql = "INSERT INTO article(title, content, userID) VALUES ('$title', '$content', '$userID')";
 $result = mysqli_query($link, $sql);
 if($result){
 ?>
 <script language="javascript">
     alert("新增文章成功");
-    location.herf="talk.php";
-    //location.herf="talkDetail.php?articleID=<?php echo $articleID;?>";
+    location.href="talk.php";
 </script>
 <?php
 }else{
